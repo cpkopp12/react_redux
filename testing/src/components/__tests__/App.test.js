8bbc455f84fe62f4b04fa9103cff55dbab30525d
cpkopp12 <cameronpkopp@gmail.com>
2023-12-26 16:29:59 -0500

    enzyme tests set up

---
+++ testing/src/components/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+import CommentBox from '../CommentBox';
+
+it('shows a comment box', () => {
+    //enzyme wrapper
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+
+});
