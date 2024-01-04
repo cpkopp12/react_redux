323735a7eb0829af5caf45616953ace90d2db914
cpkopp <cameronpkopp@gmail.com>
2024-01-04 13:21:41 -1000

    signin btn functionality

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,15 +1,57 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+// IMPORTS ------------------------------------------
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
+import * as actions from '../actions';
 
-const App = () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+// APP CLASS COMPONENT --------------------------------
+class App extends Component {
+  //signin button logic
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
+    }
+  }
 
-export default App;
+  // serves as nav bar
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  //App render
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+//REDUX ----------------------------------------
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+// EXPORT ---------------------------------------
+export default connect(mapStateToProps, actions)(App);
