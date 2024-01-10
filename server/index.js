e62634a51c22779115fe90aa1777bccc80764a03
cpkopp <cameronpkopp@gmail.com>
2024-01-09 18:54:52 -1000

    server init commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// IMPORTS ------------------------------
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+
+// APP SETUP -------------------------------
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// SERVER SETUP -----------------------------
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
---
+++ server/router.js
@@ -0,0 +1,6 @@
+// EXPORT ROUTER FUNC -----------------------
+module.exports = function (app) {
+  app.get('/', function (req, res, next) {
+    res.send(['water bottle', 'phone', 'paper']);
+  });
+};
